7fe461698439a8003b69e6a91aa76737baab8010
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-09-05 01:10:19 +0400

    DespinaCoffee

---
+++ DespinaCoffeeShop/DespinaCoffeeShop/Areas/DespinaAdmin/Controllers/BlogsController.cs
@@ -0,0 +1,42 @@
+﻿using DespinaCoffeeShop.DAL;
+using DespinaCoffeeShop.Models;
+using DespinaCoffeeShop.ViewModels.Blogs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace DespinaCoffeeShop.Areas.DespinaAdmin.Controllers
+{
+        [Area("DespinaAdmin")]
+    public class BlogsController : Controller
+    {
+        private AppDbContext _context { get; }
+        private IEnumerable<Blog> blogs;
+        public BlogsController(AppDbContext context)
+        {
+            _context = context;
+            blogs=_context.Blogs.Where(b => !b.IsDeleted);
+        }
+        public IActionResult Index()
+        {
+            return View(blogs);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BlogCreateVM blogs)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return Content("Oo");
+        }
+    }
+}
---
+++ DespinaCoffeeShop/DespinaCoffeeShop/ViewModels/Blogs/BlogCreateVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DespinaCoffeeShop.ViewModels.Blogs
+{
+    public class BlogCreateVM
+    {
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string BlogImg { get; set; }
+        [Required]
+        public string PubishedDate { get; set; }
+        [Required]
+        public string Desc { get; set; }
+        [NotMapped, Required]
+        public IFormFile ImageFile { get; set; }
+    }
+}
